41171aabf74c0bbb4f7edd40ccfbb6a78cbfb5a9
Percy Jang <joonwoo_jang@pku.edu.cn>
2024-02-07 17:09:26 +0800

    feat: Youtube clonecoding complete.

--- youtube-clonecoding/src/App.js
+++ youtube-clonecoding/src/App.js
@@ -1,12 +1,19 @@
-import { Outlet } from 'react-router-dom';
-import SearchHeader from './components/SearchHeader';
+import { Outlet } from "react-router-dom";
+import SearchHeader from "./components/SearchHeader";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { YoutubeApiProvider } from "./context/YoutubeApiContext";
 
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <>
-      <SearchHeader/>
-      <Outlet/>
+      <SearchHeader />
+      <YoutubeApiProvider>
+        <QueryClientProvider client={queryClient}>
+          <Outlet />
+        </QueryClientProvider>
+      </YoutubeApiProvider>
     </>
   );
 }
---
+++ youtube-clonecoding/src/context/YoutubeApiContext.jsx
@@ -0,0 +1,22 @@
+import { useContext, createContext } from "react";
+import Youtube from "../api/youtube";
+// import YoutubeClient from "../api/youtubeClient";
+import FakeYoutubeClient from "../api/fakeYoutubeClient";
+
+export const YoutubeApiContext = createContext();
+
+const client = new FakeYoutubeClient();
+//const client = new YoutubeClient();
+const youtube = new Youtube(client); //fakeyoutube와 변경 사용 가능
+
+export function YoutubeApiProvider({ children }) {
+  return (
+    <YoutubeApiContext.Provider value={{ youtube }}>
+      {children}
+    </YoutubeApiContext.Provider>
+  );
+}
+
+export function useYoutubeApi() {
+    return useContext(YoutubeApiContext);
+}
